412f959dda7598fb3b7b660c0053ff1b84fcb38a
Kero <461884739@qq.com>
2015-04-25 11:18:04 +0800

    remove MainActivity.java

---
+++ src/com/kkweather/app/model/Province.java
@@ -0,0 +1,33 @@
+package com.kkweather.app.model;
+
+
+public class Province {
+
+	private int id;
+	private String provinceName;
+	private String privinceCode;
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getProvinceName(){
+		return provinceName;
+	}
+	
+	public void setProvinceName(String provinceName){
+		this.provinceName = provinceName;
+	}
+	
+	public String getPrivinceCode(){
+		return privinceCode;
+	}
+	
+	public void setPrivinceCode(String provinceCode){
+		this.privinceCode = privinceCode;
+	}
+}
